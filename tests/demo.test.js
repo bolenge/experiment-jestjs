75bab6395ab5ea511a3087a22d4c9f46d4ed2b09
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2021-11-26 11:33:12 +0100

    feat add demo test

---
+++ tests/demo.test.js
@@ -0,0 +1,12 @@
+describe('Pack Demo', function () {
+  it('should sum a + b = 8', function () {
+    const a = 3
+    const b = 5
+    const sum = a + b
+    expect(sum).toBe(8)
+  })
+
+  it('Should say Hello ', function () {
+    expect('Hello !').toBe('Hello !')
+  })
+})
